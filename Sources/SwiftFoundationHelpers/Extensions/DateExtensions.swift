12527eb715e1b230991c330e83b295d63ef1a8ab
{rnantes} <{reid.nantes@gmail.com}>
2019-11-15 13:49:53 -0500

    removed redundant public in extension

--- Sources/SwiftFoundationHelpers/Extensions/DateExtensions.swift
+++ Sources/SwiftFoundationHelpers/Extensions/DateExtensions.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 public extension Date {
-    public func toString(withFormat customDateFormat: CustomDateFormat) -> String {
+    func toString(withFormat customDateFormat: CustomDateFormat) -> String {
         let dateFormatter = DateFormatter.init(customDateFormat: customDateFormat)
         return dateFormatter.string(from: self)
     }
