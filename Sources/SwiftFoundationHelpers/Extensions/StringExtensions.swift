3f86f5c3972ebe847530fa53aeecb6d346c08304
{rnantes} <{reid.nantes@gmail.com}>
2019-12-02 16:11:07 -0500

    add camelCased and snake_cased extensions

--- Sources/SwiftFoundationHelpers/Extensions/StringExtensions.swift
+++ Sources/SwiftFoundationHelpers/Extensions/StringExtensions.swift
@@ -31,4 +31,27 @@ public extension String {
         }
     }
 
+    // converts string to camelCased
+    func camelCased(with separator: Character) -> String {
+        return self.lowercased()
+            .split(separator: separator)
+            .enumerated()
+            .map { $0.offset > 0 ? $0.element.capitalized : $0.element.lowercased() }
+            .joined()
+    }
+
+    // converts a camelCased string to snake_cased
+    func snakeCased() -> String {
+        let acronymPattern = "([A-Z]+)([A-Z][a-z]|[0-9])"
+        let normalPattern = "([a-z0-9])([A-Z])"
+        return self.processCamalCaseRegex(pattern: acronymPattern)?
+            .processCamalCaseRegex(pattern: normalPattern)?.lowercased() ?? self.lowercased()
+    }
+
+    fileprivate func processCamalCaseRegex(pattern: String) -> String? {
+        let regex = try? NSRegularExpression(pattern: pattern, options: [])
+        let range = NSRange(location: 0, length: count)
+        return regex?.stringByReplacingMatches(in: self, options: [], range: range, withTemplate: "$1_$2")
+    }
+
 }
