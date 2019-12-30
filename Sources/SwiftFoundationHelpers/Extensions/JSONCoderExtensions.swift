c8ab53a7a194f5ac3ea9f8d10826240216e68c8b
{rnantes} <{reid.nantes@gmail.com}>
2019-12-30 15:43:30 -0500

    add encode to file extension

--- Sources/SwiftFoundationHelpers/Extensions/JSONCoderExtensions.swift
+++ Sources/SwiftFoundationHelpers/Extensions/JSONCoderExtensions.swift
@@ -8,6 +8,13 @@
 import Foundation
 
 public extension JSONDecoder {
+    /// initialize JSONEncoder with a dateEncodingStrategy and prettyPrint option
+    convenience init(dateDecodingStrategy customDateFormat: CustomDateFormat) {
+        self.init()
+        let dateFormatter = DateFormatter.init(customDateFormat: customDateFormat)
+        self.dateDecodingStrategy = .formatted(dateFormatter)
+    }
+
     func decode<T>(_ type: T.Type, from data: Data, usingDateDecodingStrategy customDateFormat: CustomDateFormat) throws -> T where T: Decodable {
         let dateFormatter = DateFormatter.init(customDateFormat: customDateFormat)
         self.dateDecodingStrategy = .formatted(dateFormatter)
@@ -23,7 +30,7 @@ public extension JSONDecoder {
         return try self.decode(type, from: data)
     }
 
-    // synchronously decode from a file
+    /// Synchronously decode from a file
     func decodeFromFile<T>(_ type: T.Type, fromFile fileURL: URL, usingDateDecodingStrategy customDateFormat: CustomDateFormat) throws -> T where T: Decodable {
         // open file
         let fileData = try Data.init(contentsOf: fileURL)
@@ -116,6 +123,26 @@ extension JSONEncoder {
         }
 
         return try self.encode(value)
+    }
+
+    /// initialize JSONEncoder with a dateEncodingStrategy and prettyPrint option
+    convenience init(dateEncodingStrategy customDateFormat: CustomDateFormat, isPrettyPrinted: Bool = false) {
+        self.init()
+        let dateFormatter = DateFormatter.init(customDateFormat: customDateFormat)
+        self.dateEncodingStrategy = .formatted(dateFormatter)
+
+        if isPrettyPrinted {
+            self.outputFormatting = .prettyPrinted
+        }
+    }
+
+    /// Encode  value to json and write to file
+    func encodeToFile<T>(_ value: T, fileURL: URL, options: Data.WritingOptions = []) throws where T: Encodable {
+        // encode
+        let data = try self.encode(value)
+
+        // write to file
+        try data.write(to: fileURL, options: options)
     }
 
 }
