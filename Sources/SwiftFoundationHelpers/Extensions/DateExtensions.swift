e4e04290a0615da2059f64085d84a0163cf80bd8
rnantes <reid.nantes@gmail.com>
2022-09-09 01:52:52 -0400

    convert Date.now() to use postgres-nio strategy

--- Sources/SwiftFoundationHelpers/Extensions/DateExtensions.swift
+++ Sources/SwiftFoundationHelpers/Extensions/DateExtensions.swift
@@ -7,11 +7,19 @@
 
 import Foundation
 
+
 public extension Date {
+
     static func now() -> Date {
-        let date = Date.init()
-        let roundedtimeIntervalSinceReferenceDate = date.timeIntervalSinceReferenceDate.rounded(toPlaces: 3)
-        return Date.init(timeIntervalSinceReferenceDate: roundedtimeIntervalSinceReferenceDate)
+        let _microsecondsPerSecond: Int64 = 1_000_000
+        let _secondsInDay: Int64 = 24 * 60 * 60
+        let _psqlDateStart = Date(timeIntervalSince1970: 946_684_800) // values are stored as seconds before or after midnight 2000-01-01
+        
+        let seconds = Date.init().timeIntervalSince(_psqlDateStart) * Double(_microsecondsPerSecond)
+        let microseconds = seconds
+        let secondsFinal = Double(microseconds) / Double(_microsecondsPerSecond)
+        let now = Date(timeInterval: secondsFinal, since: _psqlDateStart)
+        return now
     }
 
     func toString(withFormat customDateFormat: CustomDateFormat) -> String {
