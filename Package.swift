a6bf638116f14b1491a34026bc00f295aeaa9823
rnantes <reid.nantes@gmail.com>
2022-09-24 23:20:50 -0400

    use swift regex in RegexHelper . make RegexHelper static.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "swift-foundation-helpers",
     platforms: [
-        .macOS(.v12),
+        .macOS(.v13),
         .iOS(.v13)
     ],
     products: [
--- Sources/SwiftFoundationHelpers/Helpers/RegexHelper/RegexHelper.swift
+++ Sources/SwiftFoundationHelpers/Helpers/RegexHelper/RegexHelper.swift
@@ -6,75 +6,50 @@
 //
 
 import Foundation
+import RegexBuilder
 
 public struct RegexHelper {
 
-    public let intWithPlusOrMinusPattern = "(-|\\+)?\\d+"
-    public let doubleWithPlusOrMinusPattern = "(-|\\+)?\\d+(\\.\\d+)?"
+    public static let intWithPlusOrMinusPattern = "(-|\\+)?\\d+"
+    public static let doubleWithPlusOrMinusPattern = "(-|\\+)?\\d+(\\.\\d+)?"
 
-    public init() {}
-
-    public func matchRanges(for regexPattern: String, inString inputString: String) -> [Range<String.Index>] {
-        guard let regex = try? NSRegularExpression(pattern: regexPattern, options: [.caseInsensitive]) else {
-            //print("invalid regex")
-            return []
-        }
-
-        let range = NSRange(inputString.startIndex..., in: inputString)
-        let matches = regex.matches(in: inputString, options: [], range: range)
-
+    public static func matchRanges(for regexPattern: String, inString inputString: String) throws -> [Range<String.Index>] {
+        let regex = try Regex(regexPattern).ignoresCase(true)
+        let matches = inputString.matches(of: regex)
+        
         var matchRanges = [Range<String.Index>]()
         for match in matches {
-            matchRanges.append(Range(match.range, in: inputString)!)
+            matchRanges.append(match.range)
         }
 
         return matchRanges
     }
 
-    public func matches(for regexPattern: String, inString inputString: String) -> [String] {
-        let matchRanges = self.matchRanges(for: regexPattern, inString: inputString)
-
-        var matchingStrings = [String]()
-        for range in matchRanges {
-            matchingStrings.append(String(inputString[range]))
-        }
-
+    public static func matches(for regexPattern: String, inString inputString: String) throws -> [String] {
+        let regex = try Regex(regexPattern).ignoresCase(true)
+        let matches = inputString.matches(of: regex)
+        let matchingStrings = matches.map({ String(inputString[$0.range]) })
         return matchingStrings
     }
 
-    public func firstMatchRange(for regexPattern: String, inString inputString: String) -> Range<String.Index>? {
-        guard let regex = try? NSRegularExpression(pattern: regexPattern, options: [.caseInsensitive]) else {
-            //print("Invalid Regex Pattern: \(regexPattern)")
-            return nil
-        }
+    public static func firstMatchRange(for regexPattern: String, inString inputString: String) throws -> Range<String.Index>? {
+        let regex = try Regex(regexPattern).ignoresCase(true)
+        let match = try regex.firstMatch(in: inputString)
+        return match?.range
+    }
 
-        let range = NSRange(inputString.startIndex..., in: inputString)
-        let firstMatch = regex.firstMatch(in: inputString, options: [], range: range)
-
-        if let match = firstMatch {
-            // first match found
-            return Range(match.range, in: inputString)!
-        } else {
+    public static func firstMatch(for regexPattern: String, inString inputString: String) throws -> String? {
+        let regex = try Regex(regexPattern).ignoresCase(true)
+        let match = try regex.firstMatch(in: inputString)
+        guard let match else {
             // no match found
             return nil
         }
+        return String(inputString[match.range])
     }
 
-    public func firstMatch(for regexPattern: String, inString inputString: String) -> String? {
-        let firstMatchRange = self.firstMatchRange(for: regexPattern, inString: inputString)
-
-        if let range = firstMatchRange {
-            // match found
-            let matchingString = String(inputString[range])
-            return matchingString
-        } else {
-            // no match found
-            return nil
-        }
-    }
-
-    public func hasMatch(for regexPattern: String, inString inputString: String) -> Bool {
-        let firstMatchRange = self.firstMatchRange(for: regexPattern, inString: inputString)
+    public static func hasMatch(for regexPattern: String, inString inputString: String) throws -> Bool {
+        let firstMatchRange = try self.firstMatchRange(for: regexPattern, inString: inputString)
 
         if firstMatchRange != nil {
             return true
@@ -84,8 +59,8 @@ public struct RegexHelper {
         }
     }
 
-    public func replaceFirstMatch(for regexPattern: String, inString inputString: String, withString replacementString: String) -> String {
-        let firstMatchRange = self.firstMatchRange(for: regexPattern, inString: inputString)
+    public static func replaceFirstMatch(for regexPattern: String, inString inputString: String, withString replacementString: String) throws -> String {
+        let firstMatchRange = try self.firstMatchRange(for: regexPattern, inString: inputString)
 
         if let range = firstMatchRange {
             // match found
@@ -97,7 +72,7 @@ public struct RegexHelper {
     }
 
     /// returns a new string by replacing matches for given pattern with given replacement string
-    public func replaceMatches(for regexPattern: String, inString inputString: String, withString replacementString: String) -> String {
+    public static func replaceMatches(for regexPattern: String, inString inputString: String, withString replacementString: String) -> String {
         guard let regex = try? NSRegularExpression(pattern: regexPattern, options: []) else {
             return inputString
         }
@@ -106,57 +81,57 @@ public struct RegexHelper {
         return regex.stringByReplacingMatches(in: inputString, options: [], range: range, withTemplate: replacementString)
     }
 
-    public func allInts(inString inputString: String) -> [Int] {
+    public static func allInts(inString inputString: String) throws -> [Int] {
         // (-|\+)? -> -, + or nothing
         // \d+ -> at least one digit
         let pattern = "(-|\\+)?\\d+"
         var matchesAsInts = [Int]()
-        for matchingString in matches(for: pattern, inString: inputString) {
+        for matchingString in try matches(for: pattern, inString: inputString) {
             matchesAsInts.append(Int(matchingString)!)
         }
 
         return matchesAsInts
     }
 
-    public func firstInt(inString inputString: String) -> Int? {
+    public static func firstInt(inString inputString: String) throws -> Int? {
         // (-|\+)? -> -, + or nothing
         // \d+ -> at least one digit
         let pattern = "(-|\\+)?\\d+"
-        if let matchingString = firstMatch(for: pattern, inString: inputString) {
+        if let matchingString = try firstMatch(for: pattern, inString: inputString) {
             return Int(matchingString)
         } else {
             return nil
         }
     }
 
-    public func allDoubles(inString inputString: String) -> [Double] {
+    public static func allDoubles(inString inputString: String) throws -> [Double] {
         // (-|\+)? -> -, + or nothing
         // \d+ -> at least one digit
         // (\.\d+)? -> . and digits or nothing
         let pattern = "(-|\\+)?\\d+(\\.\\d+)?"
 
         var matchesAsDoubles = [Double]()
-        for matchingString in matches(for: pattern, inString: inputString) {
+        for matchingString in try matches(for: pattern, inString: inputString) {
             matchesAsDoubles.append(Double(matchingString)!)
         }
 
         return matchesAsDoubles
     }
 
-    public func firstDouble(inString inputString: String) -> Double? {
+    public static func firstDouble(inString inputString: String) -> Double? {
         // (-|\+)? -> -, + or nothing
         // \d+ -> at least one digit
         // (\.\d+)? -> . and digits or nothing
         let pattern = "(-|\\+)?\\d+(\\.\\d+)?"
-        if let matchingString = firstMatch(for: pattern, inString: inputString) {
+        if let matchingString = try? firstMatch(for: pattern, inString: inputString) {
             return Double.init(matchingString)
         } else {
             return nil
         }
     }
 
-    public func getTextBefore(firstInstanceOf pattern: String, inString inputString: String) -> String {
-        if let firstMatchRange = firstMatchRange(for: pattern, inString: inputString) {
+    public static func getTextBefore(firstInstanceOf pattern: String, inString inputString: String) throws -> String {
+        if let firstMatchRange = try firstMatchRange(for: pattern, inString: inputString) {
             if firstMatchRange.lowerBound > inputString.startIndex {
                 return String(inputString[inputString.startIndex..<firstMatchRange.lowerBound])
             } else {
@@ -168,8 +143,8 @@ public struct RegexHelper {
         return inputString
     }
 
-    public func getTextAfter(firstInstanceOf pattern: String, inString inputString: String) -> String {
-        if let firstMatch = firstMatchRange(for: pattern, inString: inputString) {
+    public static func getTextAfter(firstInstanceOf pattern: String, inString inputString: String) throws -> String {
+        if let firstMatch = try firstMatchRange(for: pattern, inString: inputString) {
             let textAfterStartIndex = firstMatch.upperBound
             if textAfterStartIndex < inputString.endIndex {
                 return String(inputString[textAfterStartIndex..<inputString.endIndex])
@@ -182,8 +157,8 @@ public struct RegexHelper {
         return inputString
     }
 
-    public func getTextAfter(lastInstanceOf pattern: String, inString inputString: String) -> String? {
-        guard let matchRange = matchRanges(for: pattern, inString: inputString).last else {
+    public static func getTextAfter(lastInstanceOf pattern: String, inString inputString: String) throws-> String? {
+        guard let matchRange = try matchRanges(for: pattern, inString: inputString).last else {
             return nil
         }
 
